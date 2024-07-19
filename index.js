1a6cc4642ee62465b9f0ad92b1bca0aed90cce65
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-18 21:20:19 -0400

    All local tests pass.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const elementToChange = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", () => {
+    elementToChange.textContent = "This is really cool!";
+} );
